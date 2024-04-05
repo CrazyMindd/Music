2f98007e050985d498769724c0b94107c8f0cfe0
CrazyMindd <158000808+CrazyMindd@users.noreply.github.com>
2024-04-05 07:11:22 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -193,7 +193,7 @@ async def account_login(bot: Client, m: Message):
                         time.sleep(e.x)
                         continue
                 else:
-                    Show = f"**⥥ 🄳🄾🅆🄽🄻🄾🄰🄳🄸🄽🄶⬇️⬇️... »**\n\n**📝Name »** `{name}\n❄Quality » {raw_text2}`\n\n**🔗URL »** `{url}`"
+                    Show = f"** ⬇️⬇️🄳🄾🅆🄽🄻🄾🄰🄳🄸🄽🄶⬇️⬇️...»**\n\n**📝Name »** `{name}\n❄Quality » {raw_text2}`\n\n**🔗URL »** `{url}`"
                     prog = await m.reply_text(Show)
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
